a3f2894ce09666365d711b8ad8800b7f9dad7177
Matt Snyder <matt@cranialpulse.com>
2014-03-31 10:03:02 -0400

    Enable IDO for everything and ignore autosave dir

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 (load "00keyboard-setup.el")
 (load "01ruby.el")
 (load "02autocomplete.el")
-;; (load "03smartparens.el")
+(load "03ido.el")
 (load "04projectile.el")
 (load "05indentation.el")
 (load "06yasnippet.el")
@@ -22,3 +22,25 @@
 (load "10linenumbers.el")
 (load "11haml.el")
 (load "12dashatpoint.el")
+
+;; eshell preferences
+(add-hook 'eshell-mode-hook
+          '(lambda nil
+             (let ((path))
+               (setq path "~/bin:/opt/local/bin:/usr/local/bin:/usr/bin:/bin")
+               (setenv "PATH" path))
+             (local-set-key "\C-u" 'eshell-kill-input)))
+
+(defun eshell/cl ()
+  "Command to clear the eshell buffer."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
+
+;; Org-mode settings
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
+
+(put 'downcase-region 'disabled nil)
